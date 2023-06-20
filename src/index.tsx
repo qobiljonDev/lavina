769d1842829f15c661b153c939709fd39d362c09
qobiljonDev <jumaboyevqobiljon17@gmail.com>
2023-06-20 10:46:29 +0500

    remove devTools

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { Spinner } from "components";
 import { subscribe } from "services/api";
@@ -46,6 +45,5 @@ root.render(
         />
       </Suspense>
     </Provider>
-    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
